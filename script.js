c1944fc0496e23e7dde9359e7a747e12cb0e8c3f
Spha1632 <sphamandlan1632@gmail.com>
2026-09-24 13:20:54 +0200

    Wrote logic to check for winning sequence

--- script.js
+++ script.js
@@ -59,6 +59,53 @@ function players(name, marker) {
 
 function gameController(playerOne, playerTwo) {
   
-  
+  function checkWinner(marker) {
+
+    if (marker !== "X" && marker !== "O") {
+      return false;
+    }
+
+    // Rows
+    for (let i = 0; i < 3; i++) {
+      if (
+        board[i][0] === marker &&
+        board[i][1] === marker &&
+        board[i][2] === marker
+      ) {
+        return true;
+      }
+    }
+
+    // Columns
+    for (let i = 0; i < 3; i++) {
+      if (
+        board[0][i] === marker &&
+        board[1][i] === marker &&
+        board[2][i] === marker
+      ) {
+        return true;
+      }
+    }
+
+    // Diagonal \
+    if (
+      board[0][0] === marker &&
+      board[1][1] === marker &&
+      board[2][2] === marker
+    ) {
+      return true;
+    }
+
+    // Diagonal /
+    if (
+      board[0][2] === marker &&
+      board[1][1] === marker &&
+      board[2][0] === marker
+    ) {
+      return true;
+    }
+
+    return false;
+  }
 
 }
